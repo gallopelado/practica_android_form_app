d3d5d42501ea796141b70c9eea5f0cbb6bfca611
Juan <juanftp100@gmail.com>
2021-07-18 19:15:12 -0400

    add: Menu:Diseño y agregado de archivos

--- app/src/main/java/com/cursosandroidant/form/MainActivity.kt
+++ app/src/main/java/com/cursosandroidant/form/MainActivity.kt
@@ -2,10 +2,19 @@ package com.cursosandroidant.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Vamos a inflar el menu en esta activity
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
